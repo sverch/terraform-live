eaaea898299a1c772a953b59c326199376c05f3d
Shaun Verch <me@shaunverch.com>
2017-10-01 18:54:49 -0400

    Pass the environment name from the rakefile

--- Rakefile
+++ Rakefile
@@ -1,3 +1,6 @@
 require 'rspec/core/rake_task'
+
+# TODO: run rspec for every environment
+ENV["AWSPEC_ENVIRONMENT_NAME"] = "testenv"
 RSpec::Core::RakeTask.new('spec')
 task :default => :spec
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper.rb'
 
-#env_name = ENV.fetch("AWSPEC_ENVIRONMENT_NAME")
-env_name = "test"
+env_name = ENV.fetch("AWSPEC_ENVIRONMENT_NAME")
 
 describe vpc(env_name) do
   it { should exist }
